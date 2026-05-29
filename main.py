22975ac90a2b03eb24030d72c3ea91a8b760a952
Fom Marc <fommarc5@gmail.com>
2026-05-29 14:35:45 +0200

    bonne fin d annee a tous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from calculateur import calculer_ttc
+from calculateur import calculer_ttc 
 
-prix_ttc = calculer_ttc(100)
-print(f"prix TTC : {prix_ttc}")
+prix_ttc = calculer_ttc(100) ##bonne fin d'annee a tous
+print(f"prix TTC : {prix_ttc}") 
